7ef1aafecef38f4e07470e999303be2dab0415e4
Jeff Yeung <jyeung@trewknowledge.com>
2020-07-02 11:34:55 -0400

    Class name fixes

--- src/components/header-content/HeroWpVip.js
+++ src/components/header-content/HeroWpVip.js
@@ -3,9 +3,9 @@ import wpVipLogo from '../../assets/img/logos/wp-vip-cert-logo.svg'
 
 export default() => (
   <>
-    <img class="hero-cert-img" inject-svg src={wpVipLogo} alt="WordPress VIP logo" />
-    <h1 class="hero-title">Power your digital experiences with the platform trusted by the world's top brands.</h1>
-    <p class="lead"></p>
-    <p class="hero-date"></p>
+    <img className="hero-cert-img" inject-svg src={wpVipLogo} alt="WordPress VIP logo" />
+    <h1 className="hero-title">Power your digital experiences with the platform trusted by the world's top brands.</h1>
+    <p className="lead"></p>
+    <p className="hero-date"></p>
   </>
 )
