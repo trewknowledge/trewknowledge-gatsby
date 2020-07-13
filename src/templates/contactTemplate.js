6125f40490127e717f71fd55f2fb84ae9be2328c
Jeff Yeung <jyeung@trewknowledge.com>
2020-07-13 15:53:16 -0400

    Removed some comments

--- src/templates/contactTemplate.js
+++ src/templates/contactTemplate.js
@@ -45,10 +45,7 @@ export default ({ pageContext, location }) => {
         </div>
 
         <div className="grid-container-narrow">
-          {/* <div dangerouslySetInnerHTML={{ __html: pageContext.node.content }} /> */}
-
           <ContactForm />
-
           <p className="form-helper-text">By sending this message you agree to the Privacy Policy.</p>
         </div>
     </Layout>
