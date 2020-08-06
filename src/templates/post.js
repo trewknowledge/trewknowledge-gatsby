5be022370bedfebbbcf351fde5e87b90a1113a6d
Jeff Yeung <jyeung@trewknowledge.com>
2020-08-06 10:35:01 -0400

    Fixed anchor colors on news articles and title font

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Layout from '../components/layout';
 import WorksSlider from '../components/WorksSlider'
 import contentParser from 'gatsby-wpgraphql-inline-images';
@@ -6,8 +6,8 @@ import contentParser from 'gatsby-wpgraphql-inline-images';
 export default ({ pageContext, location }) => {
   const content = pageContext.node.content;
   const pluginOptions = {
-    wordPressUrl: 'https://trewknowledge-com-develop.go-vip.net',
-    uploadsUrl: 'https://trewknowledge-com-develop.go-vip.net/wp-content/uploads/',
+    wordPressUrl: 'https://trewknowledge.com/',
+    uploadsUrl: 'https://trewknowledge.com/wp-content/uploads/',
   };
 
   const formatDate = () => {
@@ -16,14 +16,19 @@ export default ({ pageContext, location }) => {
 
   return (
     <Layout pageTitle={pageContext.pageTitle} headerStyle={pageContext.headerStyle} location={location.pathname}>
-  
+    {console.log(content)}
       <div className="grid-container-narrow article pb0">
         <article className="article-content">
-          <h2 dangerouslySetInnerHTML={{ __html: pageContext.node.title }} className="article-title"/>
-          
+          <h2 
+            dangerouslySetInnerHTML={{ __html: pageContext.node.title }} 
+            className={pageContext.pageTitle === "News" ? "article-title" : "article-section-title"} 
+          />
+
           {pageContext.pageTitle === "News" ? <p className="article-date">{formatDate()}</p> : null} 
-          
-          <div>{contentParser({ content }, pluginOptions)}</div>
+        
+          <Fragment>
+            {contentParser({ content }, pluginOptions)}
+          </Fragment>
         </article>
         {pageContext.pageTitle !== "Privacy Policy" ? <hr/> : null} 
       </div>
